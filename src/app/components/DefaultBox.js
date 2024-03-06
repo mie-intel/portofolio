d888d9b7e87295bc17b0fbd888b70997c8ce1958
mie-intel <paryapradhanika@gmail.com>
2024-03-06 21:55:46 +0700

    add page 2

--- src/app/components/DefaultBox.js
+++ src/app/components/DefaultBox.js
@@ -1,11 +1,11 @@
 const DefaultBox = (props) => {
   return (
-    <div className="relative aspect-[1003/563] bg-cyan w-full">
+    <section className="relative aspect-[1003/563] bg-cyan w-full">
       <div className="absolute mx-auto my-auto z-2 right-0 left-[1.5%] top-[2%] bottom-0 aspect-[938/505] w-[93.8%] bg-darkcyan"></div>
       <div className="absolute mx-auto my-auto z-[10] right-0 left-0 top-0 bottom-0 aspect-[938/505] w-[93.8%] bg-back">
         {props.children}
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/app/components/Skill1.js
+++ src/app/components/Skill1.js
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc";
 import DefaultBox from "./DefaultBox";
 import Image from "next/image";
 
@@ -5,20 +6,16 @@ const Skill1 = () => {
   return (
     <DefaultBox>
       <div className="w-full h-full flex flex-row justify-between">
-        <div className="relative aspect-[459/506] w-[48.9%]">
-          <Image src="perkenalan.svg" alt="" fill />
-        </div>
         <div className="relative aspect-[448/505] w-[47.7%] flex flex-col pr-[4%] text-fgcolor justify-center items-end">
-          <div className="aspect-[448/200] w-[80%] pr-[6%] text-right gap-0 flex-col text-[6vw] font-bold">
-            <span>Halo bg!</span>
+          <div className="aspect-[448/200] w-[80%] pr-[6%] text-left gap-0 flex-col text-[4.8vw] font-bold">
+            <span>Aku </span>
+            <span>cepat</span>
             <br></br>
-            <span>Aku Arya</span>
+            <span>bikin website</span>
           </div>
-          <div className="aspect-[448/50] w-[80%] pr-[6%] flex justify-center items-end flex-col">
-            <div className="text-[2.8vw] font-semibold">
-              Teknologi Informasi 23
-            </div>
-          </div>
+        </div>
+        <div className="relative aspect-[459/506] w-[48.9%]">
+          <Image src="Skill1.svg" alt="" fill />
         </div>
       </div>
     </DefaultBox>
